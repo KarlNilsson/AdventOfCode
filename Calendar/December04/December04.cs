6fe017d3cf5c702250a4242eb907247a055449c4
Karl Nilsson <karl.nilsson@avaloninnovation.com>
2019-12-05 14:15:05 +0100

    Add December04. Currently not working.

--- Calendar/December04/December04.cs
+++ Calendar/December04/December04.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using Calendar.December04;
+using System;
+using System.Text;
+
 public class December04Puzzle : Calendar.PuzzleClass
 {
+    public December04Puzzle()
+    {
+        Title = "Secure Container";
+    }
     public override string Run()
     {
-        return Title;
+        var sb = new StringBuilder();
+        var input = Tools.GetFileContents("dec4");
+        var pwGen = new PasswordGenerator(input)
+        var pwList = pwGen.GeneratePasswordList();
+        var result = pwList.Count;
+        sb.AppendLine($"\n\tPart 1: {result.ToString()}");
+        
+        return sb.ToString();
     }
 }
 
---
+++ Calendar/December04/StringExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extensions
+{
+    static class StringExtension
+    {
+        public static string IncrementPassword(this string str)
+        {
+            return (int.Parse(str) + 1).ToString();
+        }
+        public static string SetConsecutive(this string str)
+        {
+            char[] newPassword = str.ToCharArray();
+            
+            for (int i = 1; i < str.Length; i++)
+            {
+                var a = str[i];
+                var b = newPassword[i - 1];
+                if (Char.GetNumericValue(str[i]) < Char.GetNumericValue(newPassword[i-1]))
+                {
+                    newPassword = ReplaceRemaining(i, newPassword[i - 1], newPassword);
+                    //newPassword.InsertRange(i, Enumerable.Repeat(newPassword[i - 1], str.Length - i));
+                    break;
+                    //newPassword[i] = newPassword[i - 1];
+                }
+            }
+            return new string(newPassword.ToArray());
+        }
+
+        private static char[] ReplaceRemaining(int start, char c, char[] CharArray)
+        {
+            for (int i = start; i < CharArray.Length; i++)
+            {
+                CharArray[i] = c;       
+            }
+            return CharArray;
+        }
+    }
+}
